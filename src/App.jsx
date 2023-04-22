c0bb300cb735356a7151a8434902bbf689543ba1
Marcelearn <jaworskimarcel@gmail.com>
2023-04-22 20:23:26 +0200

    vite boiler-platte

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+function App() {
+
+  return (
+    
+    <div className="app">
+      <section className="side-bar"></section>
+      <section className="main"></section>
+     
+    </div>
+   
+  )
+}
+
+export default App
