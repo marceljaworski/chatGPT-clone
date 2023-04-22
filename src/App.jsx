06f63073d34b2cf474067066097b538c98ad8aef
Marcelearn <jaworskimarcel@gmail.com>
2023-04-22 20:46:52 +0200

    styles and aside component

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,11 @@ function App() {
   return (
     
     <div className="app">
-      <section className="side-bar"></section>
+      <section className="side-bar">
+        <button>+ New chat</button>
+        <ul className="history"></ul>
+        <nav>Made by Marcel</nav>
+      </section>
       <section className="main"></section>
      
     </div>
