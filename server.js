d2714259f1212cdc37208ed0afa4f44e8df09251
Marcelearn <jaworskimarcel@gmail.com>
2023-04-24 20:51:27 +0200

    max_tokens 200

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.post('/completions', async (req, res) => {
         body: JSON.stringify({
             model: "gpt-3.5-turbo",
             messages: [{ role: "user", content: req.body.message}],
-            max_tokens: 100,
+            max_tokens: 200,
         })
     }
     try {
