3eb46ef9b781dd71d61e3e664a0f63860c54cd4d
Marcelearn <jaworskimarcel@gmail.com>
2023-05-10 14:44:30 +0200

    express.static frontend from backend

--- server.js
+++ server.js
@@ -3,6 +3,17 @@ import cors from "cors";
 import dotenv from "dotenv";
 import fetch from "node-fetch";
 dotenv.config();
+
+// Da die Variable __dirname nicht unter ESM existiert, müssen wir sie selbst erzeugen.
+// Sie gibt uns den absoluten Pfad zum aktuellen Projektverzeichnis an.
+// Das brauchen wir weiter unten zum Ausliefern von Dateien.
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+
+
 const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
@@ -31,5 +42,16 @@ app.post('/completions', async (req, res) => {
         console.error(error)
     }
 })
+
+// Um das Frontend über das Backend auszuliefern, benötigen wir nur die folgenden zwei Zeilen.
+// Die erste Zeile stellt mit express.static alle(!) Dateien in einem Verzeichnis zur Verfügung (ähnlich res.sendFile).
+// Beispiel: GET /static/js/main.abcd1234.js liefert die Datei ./files/frontend/static/js/main.abcd1234.js zurück.
+// Wird eine Seite (siehe React Router) im Frontend angefragt, sorgt die zweite Zeile dafür, dass die Anfrage immer auf index.html landet.
+// -------------------------
+app.use("/", express.static("./dist"));
+app.get("/*", (req, res) => res.sendFile(__dirname + "/dist/index.html"));
+// -------------------------
+
+
 app.listen(PORT, () => console.log("Your server is running on PORT " + PORT))
 
