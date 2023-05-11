3c1fc1879be810b7e428aa06ba966b188926212e
Marcelearn <jaworskimarcel@gmail.com>
2023-05-11 12:46:36 +0200

    vite.config proxis

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+import dotenv from "dotenv";
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/completions': {
+        target: `http://localhost:${PORT}`,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
