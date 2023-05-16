34106f48e4282ee2db6811884dbe6a862a600e53
Marcelearn <jaworskimarcel@gmail.com>
2023-05-16 12:03:26 +0200

    backend render link added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -27,7 +27,7 @@ function App() {
       }
     }
     try {
-      const response = await fetch('http://localhost:8000/completions', options)
+      const response = await fetch('https://chat-gpt-clone-backend-ogmk.onrender.com/completions', options)
       const data = await response.json();
       setMessage(data.choices[0].message);
     } catch (error) {
